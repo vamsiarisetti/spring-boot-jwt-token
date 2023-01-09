91a5c359d8924fc3aa399b5d251f29eca4143a19
Vamsi Arisetti <vamsiarisetti@Vamsis-MacBook-Pro.local>
2023-01-09 11:40:02 +0530

    first commit

---
+++ src/main/java/com/javainuse/dao/UserDao.java
@@ -0,0 +1,10 @@
+package com.javainuse.dao;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+
+//@Repository
+public interface UserDao /*extends JpaRepository<DAOUser, Integer>*/
+{
+//	DAOUser findByUsername(String username);
+}
---
+++ src/main/java/com/javainuse/model/UserDTO.java
@@ -0,0 +1,12 @@
+package com.javainuse.model;
+
+//import lombok.Getter;
+//import lombok.Setter;
+
+//@Getter
+//@Setter
+public class UserDTO
+{
+	private String username;
+	private String password;
+}
